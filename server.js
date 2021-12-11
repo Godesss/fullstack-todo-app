7309c8631fee329603f9270dc733b161b1b493f0
tictac <fominsartems947@gmail.com>
2021-12-11 21:16:51 +0200

    make it better

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
     credentials:true,
-    origin: "http://localhost:5000",
+    origin: "https://todoapp231.herokuapp.com",
 }));
 
 app.use("/user",registration)
