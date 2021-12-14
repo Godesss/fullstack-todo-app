6a8e5fe6450e6ffdea9b8d8b76f50dcfe42a02ae
tictac <fominsartems947@gmail.com>
2021-12-14 23:34:00 +0200

    3

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 app.use(function(request, response, next){
 
-    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     response.header("Access-Control-Allow-Credentials", true);
