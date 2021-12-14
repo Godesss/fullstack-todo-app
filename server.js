ca929bb3c8401fee5ac1a46d78fb032079708c61
tictac <fominsartems947@gmail.com>
2021-12-14 23:01:55 +0200

    3

--- server.js
+++ server.js
@@ -11,13 +11,20 @@ import tasks from "./routes/tasks.js"
 import { errorMiddle } from "./middlewares/errorMiddle.js";
 
 const app = express();
-app.use(cors({
+/*app.use(cors({
     credentials:true,
     origin: "http://localhost:3000",
-}));
+}));*/
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+})
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(cookieParser());
 
 app.use("/user",registration)
 app.use("/methods",tasks)
