509a3b72f8bff0987db861d669c3af02cea06998
tictac <fominsartems947@gmail.com>
2022-09-02 14:30:11 +0300

    init

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
-
-import express from "express"
-import mongoose from "mongoose";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser"
 import cors from "cors"
 import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
 dotenv.config()
 
 import path from "path"
 const __dirname = path.resolve()
 
+import { errorMiddle } from "./middlewares/errorMiddle.js"
 import registration from "./routes/auth.js"
 import tasks from "./routes/tasks.js"
-import { errorMiddle } from "./middlewares/errorMiddle.js";
 
-const app = express();
+const app = express()
 
 const corsOptions = {
-    credentials:true,
-    origin:"http://localhost:3000"
+	credentials: true,
+	origin: "*",
 }
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 app.use(cors(corsOptions))
-app.use(cookieParser());
+app.use(cookieParser())
 
-app.use("/user",registration)
-app.use("/methods",tasks)
+app.use("/user", registration)
+app.use("/methods", tasks)
 app.use(errorMiddle)
-app.use(express.static(path.join(__dirname,"dist")));
+app.use(express.static(path.join(__dirname, "dist")))
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
-const start =  async () => {
-    try {
-        app.listen(process.env.PORT || 3000, ()=>{console.log("server started")})
-        await mongoose.connect(process.env.MONGO_URL);
-    }
-    catch(e) {
-        console.log(e.message)
-    }
+const start = async () => {
+	try {
+		app.listen(process.env.PORT || 3000, () => {
+			console.log("server started")
+		})
+		await mongoose.connect(process.env.MONGO_URL)
+	} catch (e) {
+		console.log(e.message)
+	}
 }
 
 start()
