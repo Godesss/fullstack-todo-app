e0b9eae7878e7b292778689e2e35d3f2663e334e
tictac <fominsartems947@gmail.com>
2022-09-02 15:37:55 +0300

    cookies fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,6 +43,7 @@ class AuthController {
 		try {
 			const { refreshToken } = req.cookies
 			console.log("refreshToken", refreshToken)
+			console.log("all cookies", req.cookies)
 			const userData = await userService.refresh(refreshToken)
 			res.cookie("refreshToken", userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
 			return res.json(userData)
