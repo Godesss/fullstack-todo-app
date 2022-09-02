f74eb3081f15df402dded95064df26ee443b31a1
tictac <fominsartems947@gmail.com>
2022-09-02 19:06:56 +0300

    ..

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors(corsOptions))
-
+app.options("*", cors(corsConfig))
 app.use("/user", registration)
 app.use("/methods", tasks)
 app.use(errorMiddle)
